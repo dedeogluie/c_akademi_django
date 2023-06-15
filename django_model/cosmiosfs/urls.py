65bbe04f1ff973d29ab367c20095d040f7f812a5
klc-pakize <pa3kize@gmail.com>
2023-06-15 22:04:30 +0300

    feat: finished project

---
+++ django_model/cosmiosfs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import (home, home2)
+
+urlpatterns = [
+    path("home/", home),
+    path("home2/", home2)
+]
---
+++ django_model/cosmiosfs/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Djnago Merhaba")
+
+
+def home2(request):
+    return HttpResponse("Merhaba")
