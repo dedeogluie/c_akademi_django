a3a049126f38d37c3e3a5dbc53b6a354f91a951f
klc-pakize <pa3kize@gmail.com>
2023-06-20 21:43:54 +0300

    feat: function basic views

--- django_view/student_api/urls.py
+++ django_view/student_api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import home, student_api, student_api_get_update_delete, path_api
+from .views import home, student_list, student_create, stundent_detail, stundet_update, student_delete, stundet_patch
 
 urlpatterns = [
     path('', home),
-    path('student/', student_api),
-    path('student/<int:pk>/', student_api_get_update_delete),
-    path("path/", path_api)
+    path("student-list/", student_list),
+    path("stundet-create/", student_create),
+    path("student-detail/<int:pk>/", stundent_detail),
+    path("student-update/<int:pk>/", stundet_update),
+    path("delete/<int:pk>/", student_delete),
+    path("patch/<int:pk>/",  stundet_patch)
+
 ]
--- django_view/student_api/views.py
+++ django_view/student_api/views.py
@@ -7,76 +7,80 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
-
-
+@api_view()
 def home(request):
-    return HttpResponse('<h1>API Page</h1>')
+    return Response({
+        "home":"burası home sayfasıdır"
+    })
 
-@api_view(['GET', 'POST'])
-def student_api(request):
-    if request.method == 'GET':
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            data = {
-                "message": f"Student {serializer.validated_data.get('first_name')} saved successfully!"}
-            return Response(data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# http methods ----------->
+# - GET (DB den veri çağırma, public)
+# - POST(DB de değişklik, create, private)
+# - PUT (DB DE KAYIT DEĞİŞKLİĞİ, private)
+# - delete (dB de kayıt silme)
+# - patch (kısmi update)
 
-@api_view(['GET', 'PUT', 'DELETE', 'PATCH'])
-def student_api_get_update_delete(request, pk):
-    student = get_object_or_404(Student, pk=pk)
-    if request.method == 'GET':
-        serializer = StudentSerializer(student)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'PUT':
-        serializer = StudentSerializer(student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            data = {
-                "message": f"Student {student.last_name} updated successfully"
-            }
-            return Response(data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'PATCH':
-        serializer = StudentSerializer(student, data=request.data,partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            data = {
-                "message": f"Student {student.last_name} updated successfully"
-            }
-            return Response(data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        student.delete()
-        data = {
-            "message": f"Student {student.last_name} deleted successfully"
+
+@api_view(['GET'])
+def student_list(request):
+    student = Student.objects.all()
+    print(student)
+    serializer = StudentSerializer(student, many = True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def student_create(request):
+    serializer = StudentSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message":"Öğrenci başarı bir şekilde kayıt edildi."
         }
-        return Response(data)
+        return Response(message, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def path_api(request):
-    # from rest_framework.decorators import api_view
-    # from rest_framework.response import Response
-    # from rest_framework import status
+@api_view(['GET'])
+def stundent_detail(request, pk): 
+    student = get_object_or_404(Student, id = pk)
+    serializer = StudentSerializer(student)
+    return Response(serializer.data)
 
-    if request.method == 'GET':
-        paths = Path.objects.all()
-        serializer = PathSerializer(paths, many=True, context={'request': request})
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        # from pprint import pprint
-        # pprint(request)
-        serializer = PathSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            data = {
-                "message": f"Path saved successfully!"}
-            return Response(data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def stundet_update(request, pk):
+   student = get_object_or_404(Student, id = pk)
+   serializer = StudentSerializer(instance=student,data = request.data)
+   if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message":"Öğrenci başarı bir şekilde değiştiril edildi."
+        }
+        return Response(message, status=status.HTTP_201_CREATED)
+   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PATCH'])
+def stundet_patch(request, pk):
+   student = get_object_or_404(Student, id = pk)
+   serializer = StudentSerializer(instance=student,data = request.data, partial=True)
+   if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message":"Öğrenci başarı bir şekilde değiştiril edildi."
+        }
+        return Response(message, status=status.HTTP_201_CREATED)
+   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['DELETE'])
+def student_delete(request, pk):
+    student = get_object_or_404(Student, id = pk)
+    student.delete()
+    message = {
+            "message":"Öğrenci başarı bir şekilde silindi."
+        }
+    return Response(message)
+
